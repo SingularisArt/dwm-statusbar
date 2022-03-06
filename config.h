c9db1552e49cae9b7dbc34889f7082afa4452963
Hashem A. Damrah <dermdorm2@gmail.com>
2022-03-05 18:24:35 -0800

    Update dwmblocks.h

--- config.h
+++ config.h
@@ -1,19 +1,17 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/																												/*Update Interval*/	  /*Update Signal*/
-	{ "",			"$HOME/Singularis/local/scripts/statusbar/sb-disk",								 600,										1   }, 
-	//{ "",			"$HOME/Singularis/local/scripts/statusbar/sb-cpubars",						 1, 										2   }, 
-	{ "",			"$HOME/Singularis/local/scripts/statusbar/sb-memory",							 60,										3   }, 
-  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-music",               1,										  4   }, 
-  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-volume",              1,										  5   }, 
-  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-mailbox",             10,									  6   }, 
-  //{ "",			"$HOME/Singularis/local/scripts/statusbar/sb-packages",           30 ,										7   }, 
-  //{ "",			"$HOME/Singularis/local/scripts/statusbar/sb-news",               5,  									  8   }, 
-  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-current-course",      5, 										9   }, 
-  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-clock",               60,										10  }, 
-  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-battery",             10,									  11  }, 
-  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-internet",            10,									  12  }, 
-  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-blurtoggle",          1, 										13  }, 
-  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-help-icon",           0,										  14  }, 
+  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-theme",              0,										  1   }, 
+	{ "",			"$HOME/Singularis/local/scripts/statusbar/sb-disk",							  600,										2   }, 
+	{ "",			"$HOME/Singularis/local/scripts/statusbar/sb-memory",						  60,										  3   }, 
+  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-music",              1,										  4   }, 
+  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-volume",             1,										  5   }, 
+  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-mailbox",            10,									    6   }, 
+  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-current-course",     5, 										  7   }, 
+  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-clock",              60,										  8   }, 
+  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-battery",            10,									    10  }, 
+  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-internet",           10,									    12  }, 
+  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-blurtoggle",         1, 										  14  }, 
+  { "",			"$HOME/Singularis/local/scripts/statusbar/sb-help-icon",          0,										  16  }, 
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
