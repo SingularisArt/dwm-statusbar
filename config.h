85b21a019e40ec74ac2f3a5b4bfb36e49df38111
Hashem A. Damrah <dermdorm2@gmail.com>
2022-04-27 06:47:21 -0700

    Update config.h

--- config.h
+++ config.h
@@ -1,19 +1,15 @@
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/																												/*Update Interval*/	  /*Update Signal*/
-	// { "",			"$HOME/Singularis/local/statusbar/sb-forecast",					  60,										  1   }, 
-	{ "",			"$HOME/Singularis/local/statusbar/sb-disk",							  60,										  3   }, 
-	{ "",			"$HOME/Singularis/local/statusbar/sb-memory",						  60,										  6   }, 
-  { "",			"$HOME/Singularis/local/statusbar/sb-music",              1,										  7   }, 
-  { "",			"$HOME/Singularis/local/statusbar/sb-volume",             1,										  8   }, 
-  { "",			"$HOME/Singularis/local/statusbar/sb-mailbox",            10,									    9   }, 
-  // { "",			"$HOME/Singularis/local/statusbar/sb-news",               60,									    10  },
-  { "",			"$HOME/Singularis/local/statusbar/sb-current-course",     5, 										  11  }, 
-  { "",			"$HOME/Singularis/local/statusbar/sb-clock",              60,										  12  }, 
-  { "",			"$HOME/Singularis/local/statusbar/sb-prayer",             60,										  13  }, 
-  { "",			"$HOME/Singularis/local/statusbar/sb-battery",            10,									    14  }, 
-  { "",			"$HOME/Singularis/local/statusbar/sb-internet",           10,									    15  }, 
-  { "",			"$HOME/Singularis/local/statusbar/sb-blurtoggle",         0, 										  16  }, 
+    {"", "$HOME/Singularis/local/statusbar/sb-music", 1, 2},
+    {"", "$HOME/Singularis/local/statusbar/sb-volume", 1, 3},
+    {"", "$HOME/Singularis/local/statusbar/sb-mailbox", 10, 4},
+    {"", "$HOME/Singularis/local/statusbar/sb-current-course", 5, 5},
+    {"", "$HOME/Singularis/local/statusbar/sb-clock", 60, 6},
+    {"", "$HOME/Singularis/local/statusbar/sb-prayer", 60, 7},
+    {"", "$HOME/Singularis/local/statusbar/sb-battery", 10, 8},
+    {"", "$HOME/Singularis/local/statusbar/sb-internet", 10, 9},
+    {"", "$HOME/Singularis/local/statusbar/sb-blurtoggle", 0, 10},
 };
 
-//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+// Sets delimiter between status commands. NULL character ('\0') means no
+// delimiter.
 static char *delim = " | ";
